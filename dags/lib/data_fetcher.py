04dd0b2c0abbcf9a50313dd1408ef5c4738dd9ea
Paul Flammarion <paul@flammarion.eu>
2024-04-30 11:39:34 +0200

    fetch_data_from_imdb

--- dags/lib/data_fetcher.py
+++ dags/lib/data_fetcher.py
@@ -1,4 +1,17 @@
-def fetch_data_from_x(** kwargs):
-    print("Bla bla on a de la data depuis internet")
-    print(".....")
-    print("Done")
+import os
+from datetime import date
+import requests
+
+DATALAKE_ROOT_FOLDER = "./datalake/"
+
+
+def fetch_data_from_imdb(**kwargs):
+    current_day = date.today().strftime("%Y%m%d")
+
+    TARGET_PATH = DATALAKE_ROOT_FOLDER + "raw/imdb/MovieRating/" + current_day + "/"
+
+    if not os.path.exists(TARGET_PATH):
+        os.makedirs(TARGET_PATH)
+    url = 'https://datasets.imdbws.com/title.ratings.tsv.gz'
+    r = requests.get(url, allow_redirects=True)
+    open(TARGET_PATH + 'title.ratings.tsv.gz', 'wb').write(r.content)
