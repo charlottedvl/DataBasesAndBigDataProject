b7461663c323b82fae5f25a389e1b59ac838da3c
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-05-02 19:04:46 +0200

    update fetch data function

--- dags/lib/data_fetcher.py
+++ dags/lib/data_fetcher.py
@@ -1,17 +1,37 @@
 import os
 from datetime import date
 import requests
+from dotenv import load_dotenv, dotenv_values
 
-DATALAKE_ROOT_FOLDER = "./datalake/"
+datalake_root_folder = "./datalake/"
 
 
-def fetch_data_from_imdb(**kwargs):
+def fetch_data_from_themuse(**kwargs):
     current_day = date.today().strftime("%Y%m%d")
 
-    TARGET_PATH = DATALAKE_ROOT_FOLDER + "raw/imdb/MovieRating/" + current_day + "/"
+    target_path = datalake_root_folder + "raw/themuse/job/" + current_day + "/"
 
-    if not os.path.exists(TARGET_PATH):
-        os.makedirs(TARGET_PATH)
-    url = 'https://datasets.imdbws.com/title.ratings.tsv.gz'
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    url = 'https://www.themuse.com/api/public/jobs?page=1'
     r = requests.get(url, allow_redirects=True)
-    open(TARGET_PATH + 'title.ratings.tsv.gz', 'wb').write(r.content)
+    open(target_path + 'jobs.announce.themuse.tsv.gz', 'wb').write(r.content)
+
+
+def fetch_data_from_findwork(**kwargs):
+    current_day = date.today().strftime("%Y%m%d")
+
+    target_path = datalake_root_folder + "raw/findwork/job/" + current_day + "/"
+    # Load the environment variables
+    load_dotenv()
+
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+
+    url = 'https://findwork.dev/api/jobs/?sort_by=relevance'
+    authorization = 'Token ' + os.environ.get("API_KEY")
+    headers = {
+        'Authorization': authorization
+    }
+    r = requests.get(url, allow_redirects=True, headers=headers)
+    open(target_path + 'jobs.announce.findwork.tsv.gz', 'wb').write(r.content)
--- test/test_data_fetcher.py
+++ test/test_data_fetcher.py
@@ -1,4 +1,4 @@
-from dags.lib.data_fetcher import fetch_data_from_imdb
+from dags.lib.data_fetcher import fetch_data_from_themuse, fetch_data_from_findwork
 from dags.lib.raw_to_fmt_imdb import convert_raw_to_formatted
 from dags.lib.combine_data import combine_data
 from datetime import date
@@ -7,4 +7,5 @@ current_day = date.today().strftime("%Y%m%d")
 filetofind = "title.ratings.tsv.gz"
 
 
-combine_data(current_day)
+fetch_data_from_themuse()
+fetch_data_from_findwork()
