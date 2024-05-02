aaeba7a16f38c4c8082b915b3368d628c37f98fe
Paul Flammarion <paul@flammarion.eu>
2024-05-02 19:00:19 +0200

    update convert to more wide point of view

--- dags/lib/raw_to_fmt_imdb.py
+++ dags/lib/raw_to_fmt_imdb.py
@@ -1,12 +1,18 @@
 import os
 import pandas as pd
 
-DATALAKE_ROOT_FOLDER = "./datalake/"
-def convert_raw_to_formatted(file_name, current_day):
-    RATING_PATH = DATALAKE_ROOT_FOLDER + "raw/imdb/MovieRating/" + current_day + "/" + file_name
-    FORMATTED_RATING_FOLDER = DATALAKE_ROOT_FOLDER + "formatted/imdb/MovieRating/" + current_day + "/"
-    if not os.path.exists(FORMATTED_RATING_FOLDER):
-        os.makedirs(FORMATTED_RATING_FOLDER)
-    df = pd.read_csv(RATING_PATH, sep='\t')
+datalake_root_folder = "./datalake/"
+
+
+def convert_raw_to_formatted(group_name, table_name, current_day, file_name):
+    path = group_name + "/" + table_name + "/" + current_day + "/"
+
+    raw_path = datalake_root_folder + "raw/" + path + file_name
+    formatted_path = datalake_root_folder + "formatted/" + path
+
+    if not os.path.exists(formatted_path):
+        os.makedirs(formatted_path)
+
+    df = pd.read_csv(raw_path, sep='\t')
     parquet_file_name = file_name.replace(".tsv.gz", ".snappy.parquet")
-    df.to_parquet(FORMATTED_RATING_FOLDER + parquet_file_name)
+    df.to_parquet(formatted_path + parquet_file_name)
